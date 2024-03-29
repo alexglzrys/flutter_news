16e34bca92304ca23abb89afd097040204b3e735
Alejandro González Reyes <alexglzrys@gmail.com>
2024-03-28 18:33:29 -0600

    configurar la aplicación para que solo se muestre en sentido vertical (portrait)

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_flutter_app/providers/navigation_provider.dart';
 import 'package:news_flutter_app/screens/tabs_screen.dart';
 import 'package:news_flutter_app/services/news_service.dart';
 import 'package:news_flutter_app/theme/news_dark.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-        // Implementar Provider como manejador de estado de la aplicación (ChangeNotifierProvider para una sola instancia, MultiProvider para multiples instancias)
-        MultiProvider(
-      // Generar las instancias singletón (gestionadas por Provider) que se usarán de forma global en la aplicación
-      // Por defecto estas instancias son lazy, es decir, se crean bajo demanda. Sin embargo, se puede especificar que se generen desde el momento que se lanza la aplicación
-      providers: [
-        ChangeNotifierProvider(create: (_) => NavigationProvider()),
-        ChangeNotifierProvider(create: (_) => NewsService())
-      ],
-      child: const NewsApp(),
-    ));
+void main() {
+  // Garantiza que los enlaces de Flutter estén listos para ser utilizados, puesto que se requieren configurar preferencias antes de lanzar la aplicación
+  WidgetsFlutterBinding.ensureInitialized();
+  // Mostrar la aplicación solo en modo vertical (boca arriba / boca abajo)
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(
+      // Implementar Provider como manejador de estado de la aplicación (ChangeNotifierProvider para una sola instancia, MultiProvider para multiples instancias)
+      MultiProvider(
+    // Generar las instancias singletón (gestionadas por Provider) que se usarán de forma global en la aplicación
+    // Por defecto estas instancias son lazy, es decir, se crean bajo demanda. Sin embargo, se puede especificar que se generen desde el momento que se lanza la aplicación
+    providers: [
+      ChangeNotifierProvider(create: (_) => NavigationProvider()),
+      ChangeNotifierProvider(create: (_) => NewsService())
+    ],
+    child: const NewsApp(),
+  ));
+}
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
